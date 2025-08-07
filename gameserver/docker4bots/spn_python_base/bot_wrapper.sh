934e19ce532b8ce186f3aa4e356383458b4e7aa4
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-08-08 00:42:46 +0200

    add python support

---
+++ gameserver/docker4bots/spn_python_base/bot_wrapper.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+set -e
+
+action="$1"
+
+echo "Hello from the SPN Python container!"
+
+case "$action" in
+	compile)
+		# copy the code in, build and move the binary out
+		cd /spnbot/spn_python_framework/
+
+		echo "There ain't no compile step in python :)"
+		# pylint /spndata/usercode.py || pylint-exit $?
+		;;
+
+	run)
+		# run the bot and allow coredumps
+		ulimit -c unlimited
+		cd /spnbot/spn_python_framework/src
+		./main.py
+		;;
+
+	doc)
+		cd /spnbot/spn_python_framework/
+		doxygen Doxyfile
+		mv doc/html/* /doc
+		;;
+
+	*)
+		echo "Invalid action: $action"
+		exit 1
+esac
