3b2ddf9f704adaa5c36bc76b15bdd43066d66883
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-08-08 02:59:58 +0200

    add more nix stuff

---
+++ default.nix
@@ -0,0 +1,41 @@
+with (import <nixpkgs> { });
+{
+  gameserver = stdenv.mkDerivation {
+    name = "spn-gameserver";
+    src = nix-gitignore.gitignoreSource [ ] ./gameserver;
+
+    nativeBuildInputs = [ cmake ];
+
+    buildInputs = [
+      eigen
+      libmysqlconnectorcpp
+    ];
+
+    env.NIX_CFLAGS_COMPILE = "-I${lib.getDev libmysqlconnectorcpp}/include/jdbc";
+
+    installPhase = ''
+      install -Dm755 GameServer -t $out/bin/
+    '';
+
+    meta.mainProgram = "GameServer";
+  };
+
+  relayserver = stdenv.mkDerivation {
+    name = "spn-relayserver";
+    src = nix-gitignore.gitignoreSource [ ] ./relayserver;
+
+    nativeBuildInputs = [ cmake ];
+
+    buildInputs = [
+      eigen
+      openssl
+      zlib
+    ];
+
+    installPhase = ''
+      install -Dm755 relayserver/RelayServer -t $out/bin/
+    '';
+
+    meta.mainProgram = "RelayServer";
+  };
+}
